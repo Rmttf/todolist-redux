7a595fb7f9bc8bfa189411ebfa91d9c07b426baa
yangzhengzhong_i <yangzhengzhong_i@didiglobal.com>
2019-04-10 19:42:06 +0800

    delete:注释

--- src/App.js
+++ src/App.js
@@ -10,79 +10,7 @@ class App extends Component {
         this.state = {
             length: 0
         };
-        // this.changeState = this.changeState.bind(this);
-        // this.deleteDate = this.deleteDate.bind(this);
-        // this.changeComplete = this.changeComplete.bind(this);
-        // this.sortByState = this.sortByState.bind(this);
     }
-
-    // changeState(input) {
-    //     if (input && input.current.state.value) {
-    //         let new_data = this.state.data,
-    //             length = this.state.length + 1;
-    //
-    //         new_data.unshift({
-    //             "id": `${length}`,
-    //             "content": input.current.state.value,
-    //             "complete": false
-    //         });
-    //
-    //         this.setState({length: length});
-    //         this.sortByState(new_data);
-    //         input.current.state.value = '';
-    //         input.current.focus();
-    //     }
-    // }
-
-    // deleteDate(id) {
-    //     if (!id){
-    //         return;
-    //     }
-    //
-    //     let new_data = this.state.data;
-    //     new_data = new_data.filter(item => item.id !== id);
-    //     this.sortByState(new_data);
-    // }
-
-    // changeComplete(id) {
-    //     if (!id){
-    //         return ;
-    //     }
-    //
-    //     let new_data = this.state.data;
-    //     new_data.find(function (d) {
-    //         if (d.id === id) {
-    //             let new_d = d;
-    //             d.complete = d.complete ? false : true;
-    //             return new_d;
-    //         }
-    //     });
-    //
-    //     this.sortByState(new_data);
-    // }
-
-    // sortByState(state) {
-    //     let unfinished = [],
-    //         finished = [];
-    //
-    //     state.forEach(function (d) {
-    //         if (d.complete){
-    //             finished.push((d));
-    //         } else {
-    //             unfinished.push(d);
-    //         }
-    //     });
-    //
-    //     // finished.sort(function (a,b) {
-    //     //     return a.id - b.id;
-    //     // });
-    //     // unfinished.sort(function (a,b) {
-    //     //     return b.id - a.id;
-    //     // });
-    //
-    //     state = unfinished.concat(finished);
-    //     this.setState({data: state});
-    // }
 
     render() {
         return (
